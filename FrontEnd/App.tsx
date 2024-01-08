069ad565214d081bc6391b1488fd3eb807fb8814
Lê Anh Tuấn <leanhtuan.svhutech@gmail.com>
2024-01-08 21:47:55 +0700

    fix className ReactNative

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-3xl text-white">Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-purple-400">
+      <Text className="text-xl text-blue-900">Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
