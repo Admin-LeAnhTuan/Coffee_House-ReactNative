813451e4c9706996175497cb879da109b2a3681d
Danh <nguyencongdanh2002@gmail.com>
2024-01-10 01:14:37 +0700

    Init HomeScreen and CartScreen

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./src/navigator/TabNabvigator";
+import DetailsScreen from "./src/screens/DetailsScreen";
+// import PaymentScreen from "./src/screens/PaymentScreen";
+import SplashScreen from "react-native-splash-screen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-purple-400">
-      <Text className="text-xl text-blue-900">Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{ animation: "slide_from_bottom" }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ animation: "slide_from_bottom" }}
+        ></Stack.Screen>
+        {/* <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{ animation: "slide_from_bottom" }}
+        ></Stack.Screen> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
