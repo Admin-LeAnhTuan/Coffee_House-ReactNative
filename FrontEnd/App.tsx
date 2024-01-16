19c348e10121ca7241777bce058700589b1af092
Rezlet~ <thanhtrollvt02@gmail.com>
2024-01-17 02:24:51 +0700

    fix little bug

--- FrontEnd/App.tsx
+++ FrontEnd/App.tsx
@@ -5,6 +5,20 @@ import TabNavigator from "./src/navigator/TabNabvigator";
 import DetailsScreen from "./src/screens/DetailsScreen";
 // import PaymentScreen from "./src/screens/PaymentScreen";
 import { useFonts } from "expo-font";
+import CategoryManageScreen from "./src/screens/admins/CategoryManageScreen";
+import ProductManageScreen from "./src/screens/admins/ProductManageScreen";
+import ToppingManageScreen from "./src/screens/admins/ToppingManageScreen";
+import OrderManageScreen from "./src/screens/admins/OrderManageScreen";
+import AddCategory from "./src/screens/admins/AddCategory";
+import EditCategory from "./src/screens/admins/EditCategory";
+import DrinkManagementScreen from "./src/screens/admins/DrinkManagementScreen";
+import AddDrinkScreen from "./src/screens/admins/AddDrinkScreen";
+import StaticScreen from "./src/screens/admins/StaticScreen";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import HomeScreen from "./src/screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,24 +36,76 @@ export default function App() {
   });
   if (!fontsLoaded) return;
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Tab"
-          component={TabNavigator}
-          options={{ animation: "slide_from_bottom" }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ animation: "slide_from_bottom" }}
-        ></Stack.Screen>
-        {/* <Stack.Screen
-          name="Payment"
-          component={PaymentScreen}
-          options={{ animation: "slide_from_bottom" }}
-        ></Stack.Screen> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Tab"
+            component={TabNavigator}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Category Manage"
+            component={CategoryManageScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Drink Manage"
+            component={DrinkManagementScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Topping Manage"
+            component={ToppingManageScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Order Manage"
+            component={OrderManageScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Add Category"
+            component={AddCategory}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Edit Category"
+            component={EditCategory}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Add Drink"
+            component={AddDrinkScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Statistic"
+            component={StaticScreen}
+            options={{ animation: "slide_from_bottom" }}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
